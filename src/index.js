23bde7e1fa403d1c4bb7ca5a50a21a632ae4041a
Fransuelton <elton6103@gmail.com>
2022-11-10 16:45:37 -0300

    update

--- src/index.js
+++ src/index.js
@@ -6,33 +6,30 @@ function calcular() {
     var altura = Number(a.value)
     var imc = massa / (altura * altura)
 
-    res.innerHTML = `IMC: ${imc.toFixed(1)}`
-
     if (massa == 0 && altura == 0) {
-        alert("ERRO 148! Verifique os dados e tente novamente!")
         res.innerHTML = `Preencha os dados corretamente!`
     }
 
     if (imc < 17) {
-        faixa.innerHTML = "Você está muito abaixo do peso"
+        res.innerHTML = `Seu IMC é: ${imc.toFixed(1)} Você está muito abaixo do peso`
     } else {
         if (imc >= 17 && imc <= 18.5) {
-            faixa.innerHTML = "Você está abaixo do peso"
+            res.innerHTML = `IMC: ${imc.toFixed(1)} Você está abaixo do peso`
         } else {
             if (imc >= 18.5 && imc <= 25) {
-                faixa.innerHTML = "Você está no peso ideal"
+                res.innerHTML = `IMC: ${imc.toFixed(1)} Você está no peso ideal`
             } else {
                 if (imc >= 25 && imc <= 30) {
-                    faixa.innerHTML = "Você está em sobrepeso"
+                    res.innerHTML = `IMC: ${imc.toFixed(1)} Você está em sobrepeso`
                 } else {
                     if (imc >= 30 && imc <= 35) {
-                        faixa.innerHTML = "você está em obesidade"
+                        res.innerHTML = `IMC: ${imc.toFixed(1)} Você está em obesidade`
                     } else {
                         if (imc >= 35 && imc <= 40) {
-                            faixa.innerHTML = "Você está em obesidade severa"
+                            res.innerHTML = `IMC: ${imc.toFixed(1)} Você está em obesidade severa`
                         } else {
                             if (imc > 40) {
-                                faixa.innerHTML = "Você está em obesidade mórbida"
+                                res.innerHTML = `IMC: ${imc.toFixed(1)} Você em obesidade mórbida`
                             }
                         }
                     }
