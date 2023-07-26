de6c02de93d25997906087fd3ec96d771576d1fe
Fransuelton <Elton6103@gmail.com>
2023-07-26 15:19:02 -0300

    script for change theme added

---
+++ src/js/index.js
@@ -0,0 +1,32 @@
+const inputs = document.querySelectorAll("input")
+console.log(inputs)
+
+const changeThemeBtn = inputs[0]
+
+changeThemeBtn.addEventListener("click", () => {
+    const header = document.querySelector("header")
+
+    if (header.classList.contains("dark-mode")) {
+        header.classList.remove("dark-mode")
+    } else {
+        header.classList.add("dark-mode")
+    }
+
+    const mainContainer = document.querySelector(".container")
+
+    if (mainContainer.classList.contains("dark-mode")) {
+        mainContainer.classList.remove("dark-mode")
+        mainContainer.classList.remove("dark-letter")
+    } else {
+        mainContainer.classList.add("dark-mode")
+        mainContainer.classList.add("dark-letter")
+    }
+
+    const footer = document.querySelector("footer")
+
+    if (footer.classList.contains("dark-footer")) {
+        footer.classList.remove("dark-footer")
+    } else {
+        footer.classList.add("dark-footer")
+    }
+})
